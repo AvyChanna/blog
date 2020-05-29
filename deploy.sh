4759f6f47847b2f77a46b6d24d040c98e012b66a
AvyChanna <30595749+AvyChanna@users.noreply.github.com>
2020-05-29 23:32:22 +0530

    Deploy task left

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,7 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-hugo
-
-gulp build
+gulp rebuild
 
 # Go To Public folder
 cd public
--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,13 @@ var babel = require('gulp-babel');
 var debug = require('gulp-debug');
 var spawn = require('child_process').spawn;
 var log = require('fancy-log');
-
+/*************** PROCESS ASSETS ***************/
 gulp.task('minify_css', function () {
 	return gulp.src('./new/**/*.css')
 		.pipe(autoprefixer())
 		.pipe(csso())
 		.pipe(gulp.dest('./public/'))
 });
-
-
-
-
 gulp.task('minify_html', function () {
 	return gulp.src(['./new/**/*.html'])
 		.pipe(htmlmin({
@@ -29,10 +25,6 @@ gulp.task('minify_html', function () {
 		}))
 		.pipe(gulp.dest('./public/'))
 });
-
-
-
-
 gulp.task('minify_js', function () {
 	return gulp.src('./new/**/*.js')
 		.pipe(babel({
@@ -45,45 +37,53 @@ gulp.task('minify_js', function () {
 		.pipe(uglify())
 		.pipe(gulp.dest('./public/'))
 });
-
-
-
-
 gulp.task('copy_others', function () {
 	return gulp.src(["./new/**/*", "!./new/**/*.{js,css,html}"])
 		.pipe(gulp.dest("./public/"))
 });
 
-
-gulp.task('build', gulp.parallel(
-	"minify_css",
-	"minify_html",
-	"minify_js",
-	"copy_others",
-));
-
-gulp.task('clean', function () {
-	return del(['./new/*', "./public/*", '!./public/.git'])
-});
-
-gulp.task('default', gulp.series('build'));
-
+/*************** HUGO TASKS ***************/
 gulp.task('serve', function (cb) {
-	var serve_spawn = spawn('hugo',['serve', '-D']);
-
+	var serve_spawn = spawn('hugo', ['serve', '-D']);
 	serve_spawn.stdout.on('data', (data) => {
-		console.log(`${data}`);
+		log(`${data}`);
 	});
-
 	serve_spawn.stderr.on('data', (data) => {
 		log(`stderr: ${data}`);
 	});
-
 	serve_spawn.on('close', (code) => {
 		log(`serve_spawn process exited with code ${code}`);
 		cb(code);
 	});
-
+	return serve_spawn;
+});
+gulp.task('publish', function (cb) {
+	var hugo_spawn = spawn('hugo');
+	hugo_spawn.stdout.on('data', (data) => {
+		log(`${data}`);
+	});
+	hugo_spawn.stderr.on('data', (data) => {
+		log(`publish stderr: ${data}`);
+	});
+	hugo_spawn.on('close', (code) => {
+		log(`======== PUBLISH DONE [code=${code}] ========`);
+		cb(code);
+	});
+	return hugo_spawn;
 });
 
-gulp.task('deploy', gulp.series('clean', 'build'))
+/*************** UTILITY TASKS ***************/
+gulp.task('clean', function () {
+	return del(['./new/*', "./public/*", '!./public/.git'])
+});
+gulp.task('build', gulp.series('publish', gulp.parallel(
+	"minify_css",
+	"minify_html",
+	"minify_js",
+	"copy_others")));
+gulp.task('rebuild', gulp.series('clean', 'build'));
+// gulp.task('github_upload', function () {});
+/*************** DEPLOY SITE ***************/
+// gulp.task('deploy', gulp.series('rebuild', 'github_upload'));
+/*************** DEFAULT TASK ***************/
+gulp.task('default', gulp.series('build'));
